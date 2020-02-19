4898fa55382654d4eef96fc7f7cfdeb0add9b6d3
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-19 02:15:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,6 @@ end
 def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
 cheese_types.each do |cheese|
   return cheese if array.include?(cheese) == true
   end 
